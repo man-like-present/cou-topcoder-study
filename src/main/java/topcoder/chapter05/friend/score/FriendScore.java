c0ed97cc012040a591517e77dfd59ca7b8f3dd79
HeeyongPark <gmldyd109@gmail.com>
2017-06-26 08:10:46 +0900

    [MIR-06-26] friendScore and Palindrome

---
+++ src/main/java/topcoder/chapter05/friend/score/FriendScore.java
@@ -0,0 +1,12 @@
+package topcoder.chapter05.friend.score;
+
+public class FriendScore {
+    public int highestScore(String[] friends) {
+        for (String friend : friends) {
+            for (int i = 0; i < friend.length(); i++) {
+                
+            }
+        }
+        return 0;
+    }
+}
