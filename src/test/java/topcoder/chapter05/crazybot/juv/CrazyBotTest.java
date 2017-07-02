ec19bf23b760be7d6a0e68eab65d61664dd1f968
juv <juvhak@gmail.com>
2017-07-03 01:15:03 +0900

    [Juv] Second CrazyBot

--- src/test/java/topcoder/chapter05/crazybot/juv/CrazyBotTest.java
+++ src/test/java/topcoder/chapter05/crazybot/juv/CrazyBotTest.java
@@ -20,6 +20,13 @@ public class CrazyBotTest {
 //        System.out.println(getProbability(14, 50, 50, 0, 0));
 //        System.out.println(getProbability(14, 25, 25, 25, 25));
 
+        CrazyBot2 crazyBot = new CrazyBot2();
+        System.out.println(crazyBot.getProbability(1, 25, 25, 25, 25));
+        System.out.println(crazyBot.getProbability(2, 25, 25, 25, 25));
+        System.out.println(crazyBot.getProbability(7, 50, 0, 0, 50));
+        System.out.println(crazyBot.getProbability(14, 50, 50, 0, 0));
+        System.out.println(crazyBot.getProbability(14, 25, 25, 25, 25));
+
         // Then
     }
 
