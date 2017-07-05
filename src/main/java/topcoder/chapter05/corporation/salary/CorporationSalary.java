8f03f97055b439f983072a4205be9f77b29c7994
juv <juvhak@gmail.com>
2017-07-06 00:19:10 +0900

    [Juv] make NumberMagicEasy and CoporationSalary package

---
+++ src/main/java/topcoder/chapter05/corporation/salary/CorporationSalary.java
@@ -0,0 +1,11 @@
+package topcoder.chapter05.corporation.salary;
+
+/**
+ * @author Juv
+ * @since 2017-07-06
+ */
+public class CorporationSalary {
+    public long totalSalary(String[] relations) {
+        return 0L;
+    }
+}
---
+++ src/main/java/topcoder/chapter05/numbermagic/NumberMagicEasy.java
@@ -0,0 +1,11 @@
+package topcoder.chapter05.numbermagic;
+
+/**
+ * @author Juv
+ * @since 2017-07-06
+ */
+public class NumberMagicEasy {
+    public int theNumber(String answer) {
+        return 0;
+    }
+}
