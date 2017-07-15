3d78ed6fb4a87191ff6b72eaf9caace43cae7709
juv <juvhak@gmail.com>
2017-07-16 01:38:52 +0900

    [Juv] Bad Neighbors

--- src/main/java/topcoder/chapter07/bad/neighbors/juv/BadNeighbors.java
+++ src/main/java/topcoder/chapter07/bad/neighbors/juv/BadNeighbors.java
@@ -6,6 +6,30 @@ package topcoder.chapter07.bad.neighbors.juv;
  */
 public class BadNeighbors {
     public int maxDonations(int[] donations) {
-        return 0;
+
+        return maxDonations(donations, 0, donations.length - 1);
+    }
+
+    private int maxDonations(int[] donations, int left, int right) {
+
+        if (left > right) {
+            return 0;
+        }
+        if (left == right) {
+            return donations[left];
+        }
+
+        int end = (right > left + 2) ? (left + 2) : right;
+
+        int max = 0;
+        for (int i = left; i <= end; i++) {
+            int newRight = (i == 0) ? right - 1 : right;
+            int sum = donations[i] + maxDonations(donations, i + 2, newRight);
+            if (max < sum) {
+                max = sum;
+            }
+        }
+
+        return max;
     }
 }
---
+++ src/test/java/topcoder/chapter07/bad/neighbors/juv/BadNeighborsTest.java
@@ -0,0 +1,87 @@
+package topcoder.chapter07.bad.neighbors.juv;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Juv
+ * @since 2017-07-16
+ */
+public class BadNeighborsTest {
+
+    private BadNeighbors badNeighbors;
+
+    @Before
+    public void setUp() throws Exception {
+        badNeighbors = new BadNeighbors();
+    }
+
+    @Test
+    public void maxDonations_case0() throws Exception {
+        // Given
+        int[] donations = {10, 3, 2, 5, 7, 8};
+        int expected = 19;
+
+        // When
+        int actual = badNeighbors.maxDonations(donations);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxDonations_case1() throws Exception {
+        // Given
+        int[] donations = { 11, 15 };
+        int expected = 15;
+
+        // When
+        int actual = badNeighbors.maxDonations(donations);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxDonations_case2() throws Exception {
+        // Given
+        int[] donations = { 7, 7, 7, 7, 7, 7, 7};
+        int expected = 21;
+
+        // When
+        int actual = badNeighbors.maxDonations(donations);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxDonations_case3() throws Exception {
+        // Given
+        int[] donations = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+        int expected = 16;
+
+        // When
+        int actual = badNeighbors.maxDonations(donations);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxDonations_case4() throws Exception {
+        // Given
+        int[] donations = { 94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61,
+                6, 237, 12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397,
+                52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72};
+        int expected = 2926;
+
+        // When
+        int actual = badNeighbors.maxDonations(donations);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+}
