373e43509f701798923e6c777b66d48e4fff7166
juv <juvhak@gmail.com>
2017-07-16 00:19:55 +0900

    [Juv] create bad neighbors & chess metric package

---
+++ src/main/java/topcoder/chapter07/bad/neighbors/juv/BadNeighbors.java
@@ -0,0 +1,11 @@
+package topcoder.chapter07.bad.neighbors.juv;
+
+/**
+ * @author Juv
+ * @since 2017-07-16
+ */
+public class BadNeighbors {
+    public int maxDonations(int[] donations) {
+        return 0;
+    }
+}
---
+++ src/main/java/topcoder/chapter07/chess/metric/ChessMetric.java
@@ -0,0 +1,11 @@
+package topcoder.chapter07.chess.metric;
+
+/**
+ * @author Juv
+ * @since 2017-07-16
+ */
+public class ChessMetric {
+    public long howMany(int size, int[] start, int[] end, int numMoves) {
+        return 0L;
+    }
+}
