ec2ab13a3a4224e6c739b86c6c8a35af4d5c36ab
juv <juvhak@gmail.com>
2017-07-23 01:45:32 +0900

    [Juv] Hands Shaking solved

--- src/test/java/topcoder/chapter07/hands/shaking/HandsShakingTest.java
+++ src/test/java/topcoder/chapter07/hands/shaking/HandsShakingTest.java
@@ -1,5 +1,7 @@
 package topcoder.chapter07.hands.shaking;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -7,13 +9,50 @@ import org.junit.Test;
  * @since 2017-07-22
  */
 public class HandsShakingTest {
-    @Test
-    public void countPerfect() throws Exception {
-        // Given
 
-        // When
+    private HandsShaking handsShaking;
 
-        // Then
+    @Before
+    public void setUp() throws Exception {
+        handsShaking = new HandsShaking();
     }
 
+    @Test
+    public void countPerfect_case0() throws Exception {
+        // Given
+        int n = 2;
+        long expected = 1L;
+
+        // When
+        long actual = handsShaking.countPerfect(n);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countPerfect_case1() throws Exception {
+        // Given
+        int n = 4;
+        long expected = 2L;
+
+        // When
+        long actual = handsShaking.countPerfect(n);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countPerfect_case2() throws Exception {
+        // Given
+        int n = 8;
+        long expected = 14L;
+
+        // When
+        long actual = handsShaking.countPerfect(n);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
