e2a3441fe002ab26d1e43f2e896e34497516ed71
juv <juvhak@gmail.com>
2017-07-22 18:06:16 +0900

    [Juv] Hands Shaking packaging

---
+++ src/main/java/topcoder/chapter07/hands/shaking/HandsShaking.java
@@ -0,0 +1,12 @@
+package topcoder.chapter07.hands.shaking;
+
+/**
+ * @author Juv
+ * @since 2017-07-22
+ */
+public class HandsShaking {
+
+    public long countPerfect(int n) {
+        return 0l;
+    }
+}
---
+++ src/test/java/topcoder/chapter07/hands/shaking/HandsShakingTest.java
@@ -0,0 +1,19 @@
+package topcoder.chapter07.hands.shaking;
+
+import org.junit.Test;
+
+/**
+ * @author Juv
+ * @since 2017-07-22
+ */
+public class HandsShakingTest {
+    @Test
+    public void countPerfect() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+}
