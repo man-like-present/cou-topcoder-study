639984eb789a450f09d8331d0f1ace104539fd74
juv <juvhak@gmail.com>
2017-07-29 16:48:09 +0900

    [Juv] Colorful Boxes And Balls package

---
+++ src/main/java/topcoder/chapter08/colorful/boxball/juv/ColorfulBoxesAndBalls.java
@@ -0,0 +1,11 @@
+package topcoder.chapter08.colorful.boxball.juv;
+
+/**
+ * @author Juv
+ * @since 2017-07-29
+ */
+public class ColorfulBoxesAndBalls {
+    public int getMaximum(int numRed, int numBlue, int onlyRed, int onlyBlue, int bothColors) {
+        return 0;
+    }
+}
