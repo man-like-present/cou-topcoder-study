bd067d7999770206326a9a37f230c87f83379e87
HeeyongPark <gmldyd109@gmail.com>
2017-07-31 00:39:37 +0900

    [MIR-07-30] Finish

---
+++ src/test/java/topcoder/chapter08/colorful/boxball/mir/ColorfulBoxesAndBallsTest.java
@@ -0,0 +1,110 @@
+package topcoder.chapter08.colorful.boxball.mir;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import topcoder.chapter08.colorful.boxball.mir.ColorfulBoxesAndBalls;
+
+/**
+ * @author Juv
+ * @since 2017-07-29
+ */
+public class ColorfulBoxesAndBallsTest {
+
+    private topcoder.chapter08.colorful.boxball.mir.ColorfulBoxesAndBalls colorfulBoxesAndBalls;
+
+    @Before
+    public void setUp() throws Exception {
+        colorfulBoxesAndBalls = new ColorfulBoxesAndBalls();
+    }
+
+    @Test
+    public void getMaximum_case0() throws Exception {
+        // Given
+        int numRed = 2;
+        int numBlue = 3;
+        int onlyRed = 100;
+        int onlyBlue = 400;
+        int bothColors = 200;
+
+        int expected = 1400;
+
+        // When
+        int actual = colorfulBoxesAndBalls.getMaximum(numRed, numBlue, onlyRed, onlyBlue, bothColors);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMaximum_case1() throws Exception {
+        // Given
+        int numRed = 2;
+        int numBlue = 3;
+        int onlyRed = 100;
+        int onlyBlue = 400;
+        int bothColors = 300;
+
+        int expected = 1600;
+
+        // When
+        int actual = colorfulBoxesAndBalls.getMaximum(numRed, numBlue, onlyRed, onlyBlue, bothColors);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMaximum_case2() throws Exception {
+        // Given
+        int numRed = 5;
+        int numBlue = 5;
+        int onlyRed = 464;
+        int onlyBlue = 464;
+        int bothColors = 464;
+
+        int expected = 4640;
+
+        // When
+        int actual = colorfulBoxesAndBalls.getMaximum(numRed, numBlue, onlyRed, onlyBlue, bothColors);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMaximum_case3() throws Exception {
+        // Given
+        int numRed = 1;
+        int numBlue = 4;
+        int onlyRed = 20;
+        int onlyBlue = -30;
+        int bothColors = -10;
+
+        int expected = -100;
+
+        // When
+        int actual = colorfulBoxesAndBalls.getMaximum(numRed, numBlue, onlyRed, onlyBlue, bothColors);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMaximum_case4() throws Exception {
+        // Given
+        int numRed = 9;
+        int numBlue = 1;
+        int onlyRed = -1;
+        int onlyBlue = -10;
+        int bothColors = 4;
+
+        int expected = 0;
+
+        // When
+        int actual = colorfulBoxesAndBalls.getMaximum(numRed, numBlue, onlyRed, onlyBlue, bothColors);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+}
---
+++ src/test/java/topcoder/chapter08/stock/history/mir/StockHistoryTest.java
@@ -0,0 +1,54 @@
+package topcoder.chapter08.stock.history.mir;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import topcoder.chapter08.stock.history.mir.StockHistory;
+
+public class StockHistoryTest {
+
+    private topcoder.chapter08.stock.history.mir.StockHistory stockHistory;
+
+    @Before
+    public void setUp() throws Exception {
+        stockHistory = new StockHistory();
+    }
+
+    @Test
+    public void stockHistory_case0() throws Exception {
+        long expected = 500;
+
+        long actual = stockHistory.maximumEarnings(1000, 0, new String[]{"10 20 30", "15 24 32"});
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void stockHistory_case1() throws Exception {
+        long expected = 0;
+
+        long actual = stockHistory.maximumEarnings(1000, 0, new String[]{"10", "9"});
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void stockHistory_case2() throws Exception {
+        long expected = 239;
+
+        long actual = stockHistory.maximumEarnings(100, 20,
+                new String[]{
+                        "40 50 60",
+                        "37 48 55",
+                        "100 48 50",
+                        "105 48 47",
+                        "110 50 52",
+                        "110 50 52",
+                        "110 51 54",
+                        "109 49 53",
+                });
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
