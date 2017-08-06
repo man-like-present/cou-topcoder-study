a3d6e4fbdd159f28dfad8b7ed879b508ab8377a1
HeeyongPark <gmldyd109@gmail.com>
2017-08-06 23:40:58 +0900

    [MIR-08-06] weird

---
+++ src/main/java/topcoder/chapter08/auto/loan/mir/AutoLoan.java
@@ -0,0 +1,7 @@
+package topcoder.chapter08.auto.loan.mir;
+
+public class AutoLoan {
+    public double interestRate(double price, double monthlyPayment, int loanTerm) {
+        return 0;
+    }
+}
