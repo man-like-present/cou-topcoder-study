bb72578495a0e92f5e48ccfcf5053e6cead3587f
mir <gmldyd109@gmail.com>
2017-08-08 07:18:39 +0900

    [MIR-08-08] not math!!!

--- src/main/java/topcoder/chapter08/auto/loan/mir/AutoLoan.java
+++ src/main/java/topcoder/chapter08/auto/loan/mir/AutoLoan.java
@@ -2,6 +2,26 @@ package topcoder.chapter08.auto.loan.mir;
 
 public class AutoLoan {
     public double interestRate(double price, double monthlyPayment, int loanTerm) {
-        return 0;
+        double maxrate = 1.0;
+        double rate = 0.5;
+        double minrate = 0.0;
+
+        while(true) {
+            double calculatedPrice = price;
+            for (int i = 0; i < loanTerm; i++) {
+                calculatedPrice *= 1 + rate;
+                calculatedPrice -= monthlyPayment;
+            }
+            if (Math.abs(calculatedPrice)< 1e-9) {
+                return rate;
+            } else {
+                if(calculatedPrice > 0) {
+                    maxrate = rate;
+                } else {
+                    minrate = rate;
+                }
+                rate = (maxrate+minrate)/2;
+            }
+        }
     }
 }
